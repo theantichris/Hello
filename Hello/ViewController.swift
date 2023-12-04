9defc86f795ce3dc0a9a3214abc0e14f2e54aa1e
Christopher Lamm <christopher.lamm@companycam.com>
2023-12-04 14:08:44 -0500

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Christopher Lamm on 12/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
